3f97dfac65f871107e9dedf3a6f6eb8b48ad906d
shinsad0907 <shinnohope.0907@gmail.com>
2024-09-28 11:13:39 +0800

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# Lấy thời gian hiện tại
-now = datetime.now()
-
-# Định dạng thời gian thành chuỗi với giờ, phút, giây
-current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-
-print("Thời gian hiện tại:", current_time)
